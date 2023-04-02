844ada485fb4ba73b7c2b0f2a77923843a4565f5
Marcus Kim <marcuskim1989@gmail.com>
2023-04-01 17:59:29 -0700

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Marcus Y. Kim on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
